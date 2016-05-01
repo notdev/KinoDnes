0df2c43766849d0d73ddfebccc3474253bb09504
notdev <notdev@users.noreply.github.com>
2016-05-01 11:43:51 +0200

    Standardize city names

--- KinoDnes/Controllers/KinoController.cs
+++ KinoDnes/Controllers/KinoController.cs
@@ -3,6 +3,9 @@ using System.Linq;
 using System.Web.Http;
 using KinoDnes.Cache;
 using KinoDnes.Models;
+using System.Text;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace KinoDnes.Controllers
 {
@@ -11,8 +14,36 @@ namespace KinoDnes.Controllers
         // GET api/kino
         public List<Cinema> Get(string city)
         {
-            var listings = ResponseCache.GetAllListings().Where(c => c.CinemaName.Contains(city)).ToList();
+            string standardizedCity = StandardizeString(city);
+
+            var listings = ResponseCache.GetAllListings().Where(c => StandardizeString(c.CinemaName).Contains(standardizedCity)).ToList();
             return listings;
+        }
+
+        private string RemoveDiacritics(string originalString)
+        {
+            var normalizedString = originalString.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+        private string StandardizeString(string str)
+        {
+            string standardizedName = str.ToLower();
+            standardizedName = Regex.Replace(standardizedName, @"\s+", "");
+            standardizedName = RemoveDiacritics(standardizedName);
+
+            return standardizedName;
         }
     }
 }
