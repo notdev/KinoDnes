e86f5f0d0ae77543a6c83280088fabf46e527b0d
notdev <notdev@users.noreply.github.com>
2016-05-01 20:48:31 +0200

    API - Return only movies not yet started today

--- KinoDnes/Controllers/KinoController.cs
+++ KinoDnes/Controllers/KinoController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using KinoDnes.Cache;
@@ -17,6 +18,7 @@ namespace KinoDnes.Controllers
             string standardizedCity = StandardizeString(city);
 
             var listings = ResponseCache.GetAllListings().Where(c => StandardizeString(c.CinemaName).Contains(standardizedCity)).ToList();
+            listings = GetCinemasWithMoviesPlayingToday(listings);
             return listings;
         }
 
@@ -44,6 +46,63 @@ namespace KinoDnes.Controllers
             standardizedName = RemoveDiacritics(standardizedName);
 
             return standardizedName;
+        }
+
+        private List<Cinema> GetCinemasWithMoviesPlayingToday(List<Cinema> cinemas)
+        {
+            var currentTime = GetCESTTimeNow();
+
+            var listingsPlayingSinceNow = new List<Cinema>();
+
+            foreach (var listing in cinemas)
+            {
+                var moviesInThisListing = new List<Movie>();
+
+                foreach (var movie in listing.Movies)
+                {
+                    var movieWithCurrentTimes = new Movie
+                    {
+                        MovieName = movie.MovieName,
+                        Flags = movie.Flags,
+                        Rating = movie.Rating,
+                        Url = movie.Url,
+                        Times = new List<string>()
+                    };
+
+                    foreach (var time in movie.Times)
+                    {
+                        int hours = int.Parse(time.Split(':').First());
+                        int minutes = int.Parse(time.Split(':').Last());
+
+                        var listingTime = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, hours, minutes, 0);
+
+                        if (listingTime > currentTime)
+                        {
+                            movieWithCurrentTimes.Times.Add($"{hours.ToString("D2")}:{minutes.ToString("D2")}");
+                        }
+                    }
+                    if (movieWithCurrentTimes.Times.Count > 0)
+                    {
+                        moviesInThisListing.Add(movieWithCurrentTimes);
+                    }
+                }
+
+                if (moviesInThisListing.Count > 0)
+                {
+                    listingsPlayingSinceNow.Add(new Cinema
+                    {
+                        CinemaName = listing.CinemaName,
+                        Movies = moviesInThisListing
+                    });
+                }
+            }
+            return listingsPlayingSinceNow;
+        }
+
+        private DateTime GetCESTTimeNow()
+        {
+            DateTime cestTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Central European Standard Time");
+            return cestTime;
         }
     }
 }
