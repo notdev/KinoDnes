c83fbe34886a8704b51c978919597d3a6ec8d12d
Dusan Obert <obert.dusan@gmail.com>
2017-11-14 21:19:31 +0100

    init

---
+++ src/KinoDnesApi/Controllers/KinoGetController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KinoDnesApi.Controllers
+{
+    public class KinoGetController : Controller
+    {
+        [HttpGet]
+        [Route("/api/kino/hello")]
+        public IActionResult Get()
+        {
+            return Ok("Hello");
+        }
+    }
+}
