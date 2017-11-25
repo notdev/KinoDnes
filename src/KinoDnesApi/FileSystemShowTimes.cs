b3c69cb1de2bd8ec42d4618bd3984854c13b39bb
Dusan Obert <obert.dusan@gmail.com>
2017-11-25 23:20:05 +0100

    Use temp path for showtime data

--- src/KinoDnesApi/FileSystemShowTimes.cs
+++ src/KinoDnesApi/FileSystemShowTimes.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using KinoDnesApi.Model;
 using Newtonsoft.Json;
 
@@ -10,17 +7,23 @@ namespace KinoDnesApi
 {
     public class FileSystemShowTimes : IFileSystemShowTimes
     {
-        private const string Filename = "showtimes.json";
+        private readonly string _filename;
+
+        public FileSystemShowTimes()
+        {
+            var tempPath = Path.GetTempPath();
+            _filename = Path.Combine(tempPath, "showtimes.json");
+        }
 
         public void Set(IEnumerable<Cinema> showtimes)
         {
             var serialized = JsonConvert.SerializeObject(showtimes);
-            File.WriteAllText(Filename, serialized);
+            File.WriteAllText(_filename, serialized);
         }
 
         public IEnumerable<Cinema> Get()
         {
-            var jsonText = File.ReadAllText(Filename);
+            var jsonText = File.ReadAllText(_filename);
             return JsonConvert.DeserializeObject<IEnumerable<Cinema>>(jsonText);
         }
     }
